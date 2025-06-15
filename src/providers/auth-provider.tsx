1553045af55394335c583200f60f6c779b15152b
shohrux Rustamov <rustamovshohrux022@gmail.com>
2025-06-15 21:23:08 +0500

    fix: telegram

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -45,7 +45,7 @@ export default function AuthProvider(props: Props): React.ReactElement {
     if (initData && initData.user) {
       localStorage.removeItem('access_token')
       localStorage.removeItem('refresh_token')
-      window.location.reload()
+      // window.location.reload()
       loginMutate({ telegram_id: initData.user.id })
     }
   }, [])
