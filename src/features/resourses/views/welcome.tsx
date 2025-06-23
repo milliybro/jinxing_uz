98970c137f8021a4fc4214cc4dceb169107bb387
shohrux Rustamov <rustamovshohrux022@gmail.com>
2025-06-23 19:13:02 +0500

    .

--- src/features/resourses/views/welcome.tsx
+++ src/features/resourses/views/welcome.tsx
@@ -53,8 +53,8 @@ export default function Welcome(): React.ReactElement {
       onClose()
       refetch()
     },
-    onError: () => {
-      message.error('Xatolik yuz berdi.')
+    onError: (err) => {
+      message.error(err)
     },
   })
   const { mutate: updateMutate } = useMutation({
@@ -189,7 +189,7 @@ export default function Welcome(): React.ReactElement {
                       />
                       <Dropdown overlay={menu} trigger={['click']}>
                         <span className="absolute right-2 top-2 bg-[#fcfcfc] w-8 h-8 flex justify-center items-center rounded-full cursor-pointer shadow">
-                          <MoreOutlined className='text-[20px]'/>
+                          <MoreOutlined className="text-[20px]" />
                         </span>
                       </Dropdown>
                       <div
